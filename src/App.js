a7dd9a6c428dee485862295d80bdc2f7991cd88e
Victor J. Rosario V <victorrosario.1890@gmail.com>
2020-04-09 11:10:01 -0400

    Subiendo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {//
-  
+import Home from './componentes/home/Home'; 
+import Nosotros from './componentes/nosotros/Nosotros';
+import Eventos from './componentes/eventos/Eventos';
+import Contacto from './componentes/contacto/Contacto';
+import Donaciones from './componentes/donaciones/Donaciones';
+
+import { Route, Switch } from 'react-router-dom';
+
+class App extends Component {
+    render(){
+       return(
+            <div>
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/nosotros' component={Nosotros} />
+                    <Route path='/eventos' component={Eventos} />
+                    <Route path='/donaciones' component={Donaciones} />
+                    <Route path='/contacto' component={Contacto} /> 
+                </Switch>
+            </div>
+       );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';//Libreria react-dom
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'; // Librería react-router-dom
+import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import {BrowserRouter as Router} from 'react-router-dom';
 
-// Páginas del Sitio Web
-import Home from './componentes/home/Home'; 
-import Nosotros from './componentes/nosotros/Nosotros';
-import Eventos from './componentes/eventos/Eventos';
-import Contacto from './componentes/contacto/Contacto';
-import Donaciones from './componentes/donaciones/Donaciones';
-
-// Configuración de la rutas del Sitio Web 
 ReactDOM.render(
-  <Router>
-    <div>
-      <Switch>
+  <Router >
+    <App/>
+  </Router >,
+document.getElementById('root'));
 
-          {/* Páginas */}
-          <Route exact path='/' component={Home} />
-          <Route path='/nosotros' component={Nosotros} />
-          <Route path='/eventos' component={Eventos} />
-          <Route path='/donaciones' component={Donaciones} />  
-          <Route path='/contacto' component={Contacto} /> 
-
-        </Switch>
-    </div>
-  </Router>,
-document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
